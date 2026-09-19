682a7d75a66898e8c40361e159f651e21abf7db2
Mor DIOUF <bayemor1793@gmail.com>
2026-09-19 18:16:16 +0000

    fix(deployment): support vercel preset et securiser les credentials supabase en production

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env["VITE_SUPABASE_URL"];
-const supabaseAnonKey = import.meta.env["VITE_SUPABASE_ANON_KEY"];
+const defaultUrl = "https://phnbjniebdxwxqcenudj.supabase.co";
+const defaultAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBobmJqbmllYmR4d3hxY2VudWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NDQzMjgsImV4cCI6MjEwNTMyMDMyOH0.Q1Up8LwDWaYoWTXsA6XmzLuB2c0Z6VyiHlTPhnvZ-sA";
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase credentials in .env file.");
-}
+const supabaseUrl = import.meta.env["VITE_SUPABASE_URL"] || defaultUrl;
+const supabaseAnonKey = import.meta.env["VITE_SUPABASE_ANON_KEY"] || defaultAnonKey;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
